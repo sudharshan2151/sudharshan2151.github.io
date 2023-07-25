24115c88686fb5c79f76810fef3ada24af64621f
sudharshan2151 <123924081+sudharshan2151@users.noreply.github.com>
2023-07-26 00:49:56 +0530

    favicon

---
+++ resources/js/index.js
@@ -0,0 +1,21 @@
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml15 .word',
+    scale: [14,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: (el, i) => 800 * i
+  }).add({
+    targets: '.ml15',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+  window.addEventListener('load', function() {
+            // Hide the loading animation
+            var loadingAnimation = document.querySelector('.sk-chase');
+            loadingAnimation.style.display = 'none';
+        });

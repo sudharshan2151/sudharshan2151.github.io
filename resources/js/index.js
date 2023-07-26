eceb8620b9c7d5f72312ce0409984b8096f99f10
sudharshan2151 <123924081+sudharshan2151@users.noreply.github.com>
2023-07-26 23:22:57 +0530

    done

--- resources/js/index.js
+++ resources/js/index.js
@@ -19,3 +19,58 @@
             var loadingAnimation = document.querySelector('.sk-chase');
             loadingAnimation.style.display = 'none';
         });
+
+        function closeMenu() {
+          // Get the checkbox element
+          var checkbox = document.querySelector('.toggler');
+          
+          // Check if the checkbox is currently checked
+          if (checkbox.checked) {
+            // Toggle the checkbox status to uncheck it (closing the menu)
+            checkbox.checked = false;
+          }
+        }
+
+        function animateWords() {
+          const words = ["Java", "Backend", "Developer"];
+          let currentWord = 0;
+          const wordElement = document.getElementById("word");
+        
+          function changeWord() {
+            wordElement.textContent = words[currentWord];
+            currentWord = (currentWord + 1) % words.length;
+          }
+        
+          setInterval(changeWord, 1000);
+        }
+
+        const h1 = document.querySelector("span.animate-charcter");
+        
+          
+        
+       
+        const animeTitle = (element) => {
+          
+          const arrayText = element.innerHTML.split("");
+          element.innerHTML = "";
+          arrayText.forEach((letter, indice) => {
+            setTimeout(() => (element.innerHTML += letter), 135 * indice);
+          });
+          element.classList.add("animate__shakeY");
+        };
+        animeTitle(h1);
+        
+        
+        
+        const observer = new IntersectionObserver((entries) => {
+          entries.forEach((entry) => {
+            entry.isIntersecting
+              ? entry.target.classList.add("show")
+              : entry.target.classList.remove("show");
+          });
+        });
+        
+        const hiddenElements = document.querySelectorAll("section.hidden");
+        
+        hiddenElements.forEach((el) => observer.observe(el));
+            

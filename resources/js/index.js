23aa94b00d87a40b73de8b43263e2b9ad1909762
sudharshan2151 <123924081+sudharshan2151@users.noreply.github.com>
2023-07-27 18:19:47 +0530

    finined

--- resources/js/index.js
+++ resources/js/index.js
@@ -14,6 +14,7 @@
 //     delay: 1000
 //   });
 
+
   window.addEventListener('load', function() {
             // Hide the loading animation
             var loadingAnimation = document.querySelector('.sk-chase');
@@ -60,17 +61,100 @@
         };
         animeTitle(h1);
         
+      
+        consoleText(['Java Backend Developer.'], 'text',['tomato','rebeccapurple','lightblue']);
+
+        function consoleText(words, id, colors) {
+          if (colors === undefined) colors = ['#fff'];
+          var visible = true;
+          var con = document.getElementById('console');
+          var letterCount = 1;
+          var x = 1;
+          var waiting = false;
+          var target = document.getElementById(id)
+          target.setAttribute('style', 'color:' + colors[0])
+          window.setInterval(function() {
         
+            if (letterCount === 0 && waiting === false) {
+              waiting = true;
+              target.innerHTML = words[0].substring(0, letterCount)
+              window.setTimeout(function() {
+                var usedColor = colors.shift();
+                colors.push(usedColor);
+                var usedWord = words.shift();
+                words.push(usedWord);
+                x = 1;
+                target.setAttribute('style', 'color:' + colors[0])
+                letterCount += x;
+                waiting = false;
+              }, 1000)
+            } else if (letterCount === words[0].length + 1 && waiting === false) {
+              waiting = true;
+              window.setTimeout(function() {
+                x = -1;
+                letterCount += x;
+                waiting = false;
+              }, 1000)
+            } else if (waiting === false) {
+              target.innerHTML = words[0].substring(0, letterCount)
+              letterCount += x;
+            }
+          }, 120)
+          window.setInterval(function() {
+            if (visible === true) {
+              con.className = 'word hidden'
+              visible = false;
         
-        const observer = new IntersectionObserver((entries) => {
-          entries.forEach((entry) => {
-            entry.isIntersecting
-              ? entry.target.classList.add("show")
-              : entry.target.classList.remove("show");
-          });
-        });
+            } else {
+              con.className = 'word'
         
-        const hiddenElements = document.querySelectorAll("section.hidden");
-        
-        hiddenElements.forEach((el) => observer.observe(el));
-            
+              visible = true;
+            }
+          }, 400)
+        }
+
+
+// background Animation
+document.addEventListener("DOMContentLoaded", function() {
+  const starCount = 45; // Number of stars to create
+  const shootingStarInterval = 8000; // Interval between shooting stars in milliseconds
+
+  function createStar() {
+    const star = document.createElement("div");
+    star.className = "star";
+    star.style.top = `${Math.random() * 100}%`;
+    star.style.left = `${Math.random() * 100}%`;
+    document.getElementById("stars").appendChild(star);
+  }
+
+  function createShootingStar() {
+    const shootingStar = document.createElement("div");
+    shootingStar.className = "shooting-star";
+    shootingStar.style.top = `${Math.random() * 100}%`;
+    shootingStar.style.left = `${Math.random() * 100}%`;
+    document.body.appendChild(shootingStar);
+    setTimeout(() => {
+      document.body.removeChild(shootingStar);
+    }, 3000);
+  }
+
+  function generateStars() {
+    for (let i = 0; i < starCount; i++) {
+      createStar();
+    }
+  }
+
+  generateStars();
+  setTimeout(createShootingStar, Math.random() * shootingStarInterval); // Initial shooting star
+
+  // Randomize shooting star interval
+  function randomizeShootingStarInterval() {
+    const interval = Math.random() * shootingStarInterval;
+    setTimeout(function() {
+      createShootingStar();
+      randomizeShootingStarInterval();
+    }, interval);
+  }
+
+  randomizeShootingStarInterval();
+});

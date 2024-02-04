fb2edca7769f656fd4deda5bdfc933b6a17c5a6c
sudharshan2151 <123924081+sudharshan2151@users.noreply.github.com>
2024-02-04 14:15:34 +0530

    resume corrected

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 import user from "../../assets/pp (1).JPG";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import ScrollAnimation from "react-animate-on-scroll";
-import resume from "../../assets/Saravana.png"
+import resumeImage from "../../assets/Sudharshan S-Resume_page-0001.jpg";
 
 const Header = () => {
   const handleGithub = () => {
@@ -14,37 +14,18 @@ const Header = () => {
     window.open("https://www.linkedin.com/in/sudharshan2151/");
   };
 
-  // const handleTwitter = () => {
-  //   window.open("https://twitter.com/Saravana_JN");
-  // };
-
-  const handleResume = () => {
+  const handleResume = (e) => {
+    // Open the Google Drive link
+    window.open("https://drive.google.com/file/d/1DaC2SZrqlzl_6UU7_-PmJZOHt50kewNv/view");
+  
+      const resumePath = resumeImage;
+      const downloadLink = document.createElement("a");
+      downloadLink.href = resumePath;
+      downloadLink.download = "Sudharshan S-Resume_page-0001.jpg";
+      downloadLink.click();
     
-    fetch("https://drive.google.com/file/d/1DaC2SZrqlzl_6UU7_-PmJZOHt50kewNv/view")
-    .then((response) => response.blob())
-    .then((blob) => {
-      // Create a URL object from the blob
-      const url = URL.createObjectURL(blob);
-      // Create a temporary <a> element
-      const a = document.createElement("a");
-      // Set the href attribute to the URL object
-      a.href = url;
-      // Set the download attribute to the file name
-      a.download = resume;
-      // Append the <a> element to the document body
-      document.body.appendChild(a);
-      // Click the <a> element
-      a.click();
-      // Remove the <a> element from the document body
-      window.open("https://drive.google.com/file/d/1DaC2SZrqlzl_6UU7_-PmJZOHt50kewNv/view");
-      document.body.removeChild(a);
-    })
-    .catch((error) => {
-      // Handle any errors
-      console.error(error);
-    });
   };
- 
+
   return (
     <div className={styles.container}>
       <div className={styles.content_container}>
@@ -52,19 +33,13 @@ const Header = () => {
         <div className={styles.content}>
           <div className={styles.title}>
             Hi, I am <br />
-            <ScrollAnimation
-              duration={2}
-              animateOnce={true}
-              animateIn='flipInY'
-            >
+            <ScrollAnimation duration={2} animateOnce={true} animateIn='flipInY'>
               Sudharshan S
             </ScrollAnimation>
           </div>
-          <div className={styles.description}>
-            Programmer | Java Backend Developer
-          </div>
+          <div className={styles.description}>Programmer | Java Backend Developer</div>
           <div>
-            <button onClick={handleResume} >Resume</button>
+            <button onClick={handleResume}>Resume</button>
           </div>
           <div className={styles.connect}>
             <FaGithub onClick={handleGithub} size='1.5em' />
